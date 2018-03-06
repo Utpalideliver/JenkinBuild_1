a26adb6bf69f15bb3b9f4c916dc7ff02e61ea7ae
Utpalideliver <utpalendra@ideliver-inc.com>
2018-03-06 19:55:18 +0530

    Build Edit

---
+++ JenkinBuildApp_1/JenkinBuildApp_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinBuildApp_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createmessage());
+        }
+        public static String createmessage()
+        {
+            return "Hello Jenkins";
+        }
+    }
+}
---
+++ JenkinBuildApp_1/JenkinTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JenkinBuildApp_1;
+
+namespace JenkinTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello Jenkins", Program.createmessage());
+        }
+    }
+}
