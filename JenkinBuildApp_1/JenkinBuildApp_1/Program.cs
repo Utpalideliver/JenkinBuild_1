cf1b49e0f6e7f39488d58bc65836b5f9751b6ad3
Utpalideliver <utpalendra@ideliver-inc.com>
2018-03-27 11:32:38 -0500

    Updating result to Hello Jenkins1

--- JenkinBuildApp_1/JenkinBuildApp_1/Program.cs
+++ JenkinBuildApp_1/JenkinBuildApp_1/Program.cs
@@ -15,7 +15,7 @@ namespace JenkinBuildApp_1
         }
         public static String createmessage()
         {
-            return "Hello Jenkins";
+            return "Hello Jenkins1";
         }
         public static String createmessage_1()
         {
